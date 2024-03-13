1aaa047d7e841d0d13a6a29613c9929cf890cba9
jllloydd <wayfivet@gmail.com>
2024-03-13 15:58:55 +0800

    Fixed student views to reflect the sidebar for students

--- resources/views/student/MHR.blade.php
+++ resources/views/student/MHR.blade.php
@@ -16,7 +16,7 @@
         data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
 
         @include('layouts.topbar')
-        @include('layouts.sidebar')
+        @include('layouts.studentsidebar')
 
         <div class="page-wrapper">
 
